c6d01a5d9fd3cfa590d6a39219a85c5f033c0f90
alexmagikov <alexmagikov228@gmail.com>
2025-02-16 21:32:43 +0300

    forward conversion

---
+++ bwalgo/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BWalgo
+{
+    public class BWalgo
+    {
+        static int Main(string[] args)
+        {
+            var str = System.Console.ReadLine();
+            if (str is null)
+            {
+                Console.WriteLine("error: string is null");
+                return -1;
+            }
+            str = BWTalgo.ForwardConversion(str).Item1;
+            return 0;
+        }
+    }
+
+    public class Tests
+    {
+
+    }
+}
