6f8bb5e69dc61948e9f6e941779818784b7b48d1
alexmagikov <alexmagikov228@gmail.com>
2025-02-18 23:13:49 +0300

    forward conversion with table

---
+++ bwalgo/bwt.cs
@@ -0,0 +1,88 @@
+﻿public class BWTalgo
+{
+    public static (string, int) ForwardConversion(string str)
+    {
+        var permutations = new (string, int)[str.Length];
+        var endCharPosition = str.Length;
+        for (int i = 0; i < str.Length; i++)
+        {
+            permutations[i] = (str.Substring(i) + str.Substring(0, i), endCharPosition - i);
+        }
+
+        Array.Sort(permutations, new StringWithEndIndexesCompaper());
+
+        string result = "";
+        int resultEndIndex = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            result = result + permutations[i].Item1[^1];
+            if (permutations[i].Item2 == endCharPosition)
+            {
+                resultEndIndex = i + 1;
+            }
+        }
+
+        return (result, resultEndIndex);
+    }
+
+    public static string InverseConversion(string str, int endIndex)
+    {
+        endIndex--;
+        var arrayOfCountCharsBefore = new int[str.Length];
+        Dictionary<char, int> dictionaryOfCharCount = new Dictionary<char, int>();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (!dictionaryOfCharCount.ContainsKey(str[i]))
+            {
+                dictionaryOfCharCount[str[i]] = 0;
+            }
+            arrayOfCountCharsBefore[i] = dictionaryOfCharCount[str[i]];
+            dictionaryOfCharCount[str[i]]++;
+        }
+
+        Dictionary<char, int> dictionaryOfCharCountOfSmallerChars = new Dictionary<char, int>();
+        var sortedArrayByDictionaryOfCharCount = new List<char>(dictionaryOfCharCount.Keys);
+        sortedArrayByDictionaryOfCharCount.Sort();
+        var sum = 0;
+        foreach (char ch in sortedArrayByDictionaryOfCharCount)
+        {
+            dictionaryOfCharCountOfSmallerChars[ch] = sum;
+            sum += dictionaryOfCharCount[ch];
+        }
+
+        string result = "";
+        result = result + str[endIndex];
+        int lastNumRowInSortedTable = endIndex;
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            lastNumRowInSortedTable = dictionaryOfCharCountOfSmallerChars[result[0]] + arrayOfCountCharsBefore[lastNumRowInSortedTable];
+            result = str[lastNumRowInSortedTable] + result; 
+        }
+
+        return result;
+    }
+}
+
+public class StringWithEndIndexesCompaper : IComparer<(string, int)>
+{
+    public int Compare((string, int) str1, (string, int) str2)
+    {
+        int i = 0;
+        while (str1.Item1[i] == str2.Item1[i])
+        {
+            if (i == str1.Item2 || i == str2.Item2)
+            {
+                return str1.Item2 > str2.Item2 ? 1 : -1;
+            }
+            i++;
+        }
+
+        if (i == str1.Item2 || i == str2.Item2)
+        {
+            return i == str1.Item2 ? -1 : 1;
+        }
+
+        return str1.Item1[i].CompareTo(str2.Item1[i]);
+    }
+}
--- bwalgo/main.cs
+++ bwalgo/main.cs
@@ -7,19 +7,110 @@ namespace BWalgo
     {
         static int Main(string[] args)
         {
-            var str = System.Console.ReadLine();
+            if (!Tests.IsAllTestsCompleted())
+            {
+                return -1;
+            }
+
+            Console.WriteLine("choose a type of conversion: 1 - forward, 2 - inverse:");
+            var choice = Convert.ToInt32(Console.ReadLine());
+
+            return choice switch
+            {
+                1 => ProccessingForwardConversion(),
+                2 => ProccessingInverseConversion(),
+                _ => -1
+            };
+        }
+
+        static private int ProccessingForwardConversion()
+        {
+            Console.WriteLine("input string: ");
+            var str = Console.ReadLine();
             if (str is null)
             {
                 Console.WriteLine("error: string is null");
                 return -1;
             }
-            str = BWTalgo.ForwardConversion(str).Item1;
+            var result = BWTalgo.ForwardConversion(str);
+            var transformedStr = result.Item1;
+            var numOfInputStringMatrix = result.Item2;
+            Console.WriteLine($"transformed string: {transformedStr}");
+            Console.WriteLine($"num of input string in matrix: {numOfInputStringMatrix}");
+            return 0;
+        }
+
+        static private int ProccessingInverseConversion()
+        {
+            Console.WriteLine("transformed string: ");
+            var transformedStr = Console.ReadLine();
+            if (transformedStr is null)
+            {
+                Console.WriteLine("error: string is null");
+                return -1;
+            }
+            Console.WriteLine("num of input string in matrix: ");
+            var numOfInputStringMatrix = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"the original string is {BWTalgo.InverseConversion(transformedStr, numOfInputStringMatrix)}");
+
             return 0;
         }
     }
 
     public class Tests
     {
+        private static bool TestForNormalValueForForwardConversion()
+        {
+            string str = "abacaba";
+            return BWTalgo.ForwardConversion(str).Item1 == "bcabaaa" && BWTalgo.ForwardConversion(str).Item2 == 3;
+        }
 
+        private static bool TestForNormalValueForInverseConversion()
+        {
+            string str = "nnbaaa";
+            int numOfInputStringMatrix = 4;
+            return BWTalgo.InverseConversion(str, numOfInputStringMatrix) == "banana";
+        }
+
+        private static bool TestForIdenticalConversion()
+        {
+            var result = BWTalgo.ForwardConversion("banana");
+            return BWTalgo.InverseConversion(result.Item1, result.Item2) == "banana";
+        }
+
+        private static bool TestForNullStringForwardConversion()
+        {
+            var result = BWTalgo.ForwardConversion("");
+            return result.Item1 == "" && result.Item2 == 0;
+        }
+
+        private static bool TestForStringWithDublicate()
+        {
+            var result = BWTalgo.ForwardConversion("bb");
+            return result.Item1 == "bb" && result.Item2 == 2;
+        }
+
+        public static bool IsAllTestsCompleted()
+        {
+            bool[] array =
+            {
+                TestForNormalValueForForwardConversion(),
+                TestForNormalValueForInverseConversion(),
+                TestForIdenticalConversion(),
+                TestForNullStringForwardConversion(),
+                TestForStringWithDublicate(),
+            };
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (!array[i])
+                {
+                    Console.WriteLine($"test {i} is not completed");
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
