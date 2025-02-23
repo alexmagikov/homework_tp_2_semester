16bf447a12becc92b8e2d75fa7f861221462cefd
alexmagikov <alexmagikov228@gmail.com>
2025-02-23 18:21:58 +0300

    full homework

---
+++ trie/Tests.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="Tests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TrieProject;
+
+/// <summary>
+/// class for testing.
+/// </summary>
+public class Tests
+{
+    /// <summary>
+    /// check for tests.
+    /// </summary>
+    /// <returns>return true if all tests are completed, else - false.</returns>
+    public static bool IsAllTestsCompleted()
+    {
+        bool[] tests =
+        {
+            TestForNormalValueForAdd(),
+            TestForNormalValueForContains(),
+            TestForNullValueForAdd(),
+            TestForNormalValueForRemove(),
+            TestForSizeValueForAdd(),
+            TestForSizeValueForRemove(),
+            TestForHowManyStartsWithPrefixe(),
+            TestForHowManyStartsWithPrefixeForNullValue(),
+        };
+
+        foreach (var test in tests)
+        {
+            if (!test)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool TestForNormalValueForAdd()
+    {
+        Trie trie = new ();
+        trie.Add("exmpl");
+        return trie != null;
+    }
+
+    private static bool TestForNullValueForAdd()
+    {
+        Trie trie = new ();
+        return !trie.Add(string.Empty);
+    }
+
+    private static bool TestForSizeValueForAdd()
+    {
+        Trie trie = new ();
+        trie.Add("smthng1");
+        trie.Add("smthng2");
+        return trie.Size == 2;
+    }
+
+    private static bool TestForNormalValueForContains()
+    {
+        Trie trie = new ();
+        trie.Add("exmpl");
+        return trie.Contains("exmpl");
+    }
+
+    private static bool TestForNormalValueForRemove()
+    {
+        Trie trie = new ();
+        trie.Add("exmpl");
+        trie.Remove("exmpl");
+        return !trie.Contains("exmpl");
+    }
+
+    private static bool TestForSizeValueForRemove()
+    {
+        Trie trie = new ();
+        trie.Add("smthng1");
+        trie.Add("smthng2");
+        trie.Remove("smthng1");
+        return trie.Size == 1;
+    }
+
+    private static bool TestForHowManyStartsWithPrefixe()
+    {
+        Trie trie = new ();
+        trie.Add("smthng1");
+        trie.Add("smthng2");
+        return trie.HowManyStartsWithPrefix("smthng") == 2;
+    }
+
+    private static bool TestForHowManyStartsWithPrefixeForNullValue()
+    {
+        Trie trie = new ();
+        return trie.HowManyStartsWithPrefix(string.Empty) == 0;
+    }
+}
---
+++ trie/Trie.cs
@@ -0,0 +1,157 @@
+﻿// <copyright file="Trie.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TrieProject;
+
+/// <summary>
+/// class trie with methods: Add, Contains, Remove and HowManyStartsWithPrefix and property: size.
+/// </summary>
+public class Trie
+{
+    /// <summary>
+    /// root of trie.
+    /// </summary>
+    private readonly Vertex root = new ();
+
+    /// <summary>
+    /// Gets or sets size of trie.
+    /// </summary>
+    public int Size { get; set; }
+
+    /// <summary>
+    /// add string to trie.
+    /// </summary>
+    /// <param name="element">string.</param>
+    /// <returns>true if string is the first. </returns>
+    public bool Add(string element)
+    {
+        if (string.IsNullOrEmpty(element))
+        {
+            return false;
+        }
+
+        var index = 0;
+        var currentVertex = this.root;
+        while (index < element.Length)
+        {
+            if (!currentVertex.NextVertexes.ContainsKey(element[index]))
+            {
+                currentVertex.NextVertexes[element[index]] = new ();
+            }
+
+            currentVertex = currentVertex.NextVertexes[element[index]];
+            index++;
+        }
+
+        if (!currentVertex.IsTerminal)
+        {
+            this.Size++;
+            currentVertex.IsTerminal = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// return is string contained in trie.
+    /// </summary>
+    /// <param name="element">string.</param>
+    /// <returns>true if string is contained, else - false.</returns>
+    public bool Contains(string element)
+    {
+        if (string.IsNullOrEmpty(element))
+        {
+            return false;
+        }
+
+        var index = 0;
+        var currentVertex = this.root;
+        while (index < element.Length)
+        {
+            if (!currentVertex.NextVertexes.ContainsKey(element[index]))
+            {
+                return false;
+            }
+
+            currentVertex = currentVertex.NextVertexes[element[index]];
+            index++;
+        }
+
+        return currentVertex.IsTerminal;
+    }
+
+    /// <summary>
+    /// remove element from tree.
+    /// </summary>
+    /// <param name="element">element.</param>
+    /// <returns>true if element is contained, else - false.</returns>
+    public bool Remove(string element)
+    {
+        if (string.IsNullOrEmpty(element))
+        {
+            return false;
+        }
+
+        var index = 0;
+        var currentVertex = this.root;
+        while (index < element.Length)
+        {
+            if (!currentVertex.NextVertexes.ContainsKey(element[index]))
+            {
+                return false;
+            }
+
+            currentVertex = currentVertex.NextVertexes[element[index]];
+            index++;
+        }
+
+        if (currentVertex.IsTerminal)
+        {
+            currentVertex.IsTerminal = false;
+        }
+
+        this.Size--;
+        return true;
+    }
+
+    /// <summary>
+    /// return num of strings which started from prefix.
+    /// </summary>
+    /// <param name="prefix">prefix.</param>
+    /// <returns>num of strings.</returns>
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        var index = 0;
+        var currentVertex = this.root;
+        while (index < prefix.Length)
+        {
+            if (!currentVertex.NextVertexes.ContainsKey(prefix[index]))
+            {
+                return 0;
+            }
+
+            currentVertex = currentVertex.NextVertexes[prefix[index]];
+            index++;
+        }
+
+        return currentVertex.NextVertexes.Count;
+    }
+
+    /// <summary>
+    /// vertex of trie.
+    /// </summary>
+    public class Vertex
+    {
+        /// <summary>
+        /// Gets or sets the dictionary of next vertices, indexed by character.
+        /// </summary>
+        public Dictionary<char, Vertex> NextVertexes { get; set; } = new ();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this vertex is a terminal (end of a word).
+        /// </summary>
+        public bool IsTerminal { get; set; } = false;
+    }
+}
