907fb026b70d585e150af0801f4f8558f3413143
Alex Magikov <alexmagikov228@gmail.com>
2025-03-22 13:29:06 +0300

    init proj with ci

---
+++ Routers/RoutersTests/PrimTest.cs
@@ -0,0 +1,15 @@
+﻿namespace RoutersTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
