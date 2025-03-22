d1931a3e9e5f471f89327bd1bf7ff7869b70d3c9
Alex Magikov <alexmagikov228@gmail.com>
2025-03-22 20:41:58 +0300

    add FileReader

---
+++ Routers/Routers/PrimFindMst.cs
@@ -0,0 +1,22 @@
+// <copyright file="PrimFindMst.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Routers;
+
+/// <summary>
+/// Search MST.
+/// </summary>
+public class PrimFindMst
+{
+    /// <summary>
+    /// Find MST by Prim`s algorithm.
+    /// </summary>
+    /// <param name="graph">Input graph.</param>
+    public static void FindMst(Dictionary<int, List<(int Router, int LengthEdge)>> graph)
+    {
+        /*
+         * todo
+         */
+    }
+}
--- Routers/Routers/Program.cs
+++ Routers/Routers/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-Console.WriteLine("alarm!");
+using Routers;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: program <inputPath> <outputPath>");
+    return 1;
+}
+
+var inputPath = args[0];
+var outputPath = args[1];
+
+try
+{
+    var graph = CreateGraph.ReadFile(inputPath);
+    PrimFindMst.FindMst(graph);
+}
+catch (FileNotFoundException exception)
+{
+    Console.WriteLine($"File not found: {exception.FileName}");
+    return 1;
+}
+catch (FormatException exception)
+{
+    Console.WriteLine(exception.Message);
+    return 1;
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+    return 1;
+}
+
+return 0;
